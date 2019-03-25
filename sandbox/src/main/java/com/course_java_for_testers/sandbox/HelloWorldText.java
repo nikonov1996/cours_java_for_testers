fd0334732419c944c808f42d34d54ccbb3cdeac7
Nikonov Vladislav <ibigmeat1996@gmail.com>
2019-03-25 12:19:45 +0700

    rename class

--- sandbox/src/main/java/com/course_java_for_testers/sandbox/HelloWorldText.java
+++ sandbox/src/main/java/com/course_java_for_testers/sandbox/HelloWorldText.java
@@ -1,6 +1,6 @@
 package com.course_java_for_testers.sandbox;
 
-public class helloWorldText {
+public class HelloWorldText {
  public static void main(String[] args){
  System.out.println("Hello, World!!!");
 }
