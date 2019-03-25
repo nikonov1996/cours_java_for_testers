eeadf5a6dad76fcc806fca17be694fd06b372df8
Nikonov Vladislav <ibigmeat1996@gmail.com>
2019-03-25 12:07:24 +0700

    создан пакет для первой программы

--- sandbox/src/main/java/com/course_java_for_testers/sandbox/helloWorldText.java
+++ sandbox/src/main/java/com/course_java_for_testers/sandbox/helloWorldText.java
@@ -1,4 +1,6 @@
-public class textHelloWorld {
+package com.course_java_for_testers.sandbox;
+
+public class helloWorldText {
  public static void main(String[] args){
  System.out.println("Hello, World!!!");
 }
