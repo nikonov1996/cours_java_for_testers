04d80c4034080fe3fc23a8245f41d2041fc2dde7
Nikonov Vladislav <ibigmeat1996@gmail.com>
2019-03-25 15:33:45 +0700

    add some functions:
    - hi();
    - area();

--- sandbox/src/main/java/com/course_java_for_testers/sandbox/HelloWorldText.java
+++ sandbox/src/main/java/com/course_java_for_testers/sandbox/HelloWorldText.java
@@ -2,6 +2,23 @@ package com.course_java_for_testers.sandbox;
 
 public class HelloWorldText {
  public static void main(String[] args){
- System.out.println("Hello, World!!!");
+ String fio = "Nikonov Vladislav Alekseevich";
+  Hi(fio);
+  double x = 8;
+  double y = 5.1;
+  System.out.println("Area of square, with side's size " + x + " = " + area(x) );
+  System.out.println("Area of square, with side's sizes " + x + " and " + y + " = " + area(x,y) );
 }
+
+ public static void Hi (String user) {
+  System.out.println("Hello, " + user + " !!!");
+ }
+
+ public static double area (double x){
+  return x*x;
+ }
+
+ public static double area (double x, double y){
+  return x*y;
+ }
 }
