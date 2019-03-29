ecdba772ffaba5739a1983eb929709552485b0f4
Nikonov Vladislav <ibigmeat1996@gmail.com>
2019-03-29 22:02:29 +0700

    добавлен тест на создание контакта

--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/GroupCreationTests.java
@@ -1,21 +1,22 @@
 package com.course_java_for_testers.addressbook;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import static org.testng.Assert.assertTrue;
 
 public class GroupCreationTests {
-
-    @Test
-    public void GroupCreationTests() {
+    FirefoxDriver driver;
+//toDo первым выполняется второй метод,разобраться
+    @BeforeTest
+    public void setUp(){
         System.setProperty("webdriver.gecko.driver", "C:\\SeleniumGecko\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         String url = "http://localhost:8080/addressbook/group.php";
-        String url1 = "http://www.google.com/";
         driver.get(url);
         driver.findElement(By.name("user")).click();
         driver.findElement(By.name("user")).clear();
@@ -24,6 +25,11 @@ public class GroupCreationTests {
         driver.findElement(By.name("pass")).clear();
         driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+    }
+
+    @Test
+    public void testGroupCreation() {
+        driver.findElement(By.linkText("groups")).click();
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).clear();
@@ -36,6 +42,55 @@ public class GroupCreationTests {
         driver.findElement(By.name("group_footer")).sendKeys("test3");
         driver.findElement(By.name("submit")).click();
         driver.findElement(By.linkText("group page")).click();
+
+
+    }
+
+    @Test
+    public void testContactCreation(){
+        driver.findElement(By.linkText("add new")).click();
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).clear();
+        driver.findElement(By.name("firstname")).sendKeys("testname");
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).clear();
+        driver.findElement(By.name("lastname")).sendKeys("testlastname");
+        driver.findElement(By.name("nickname")).click();
+        driver.findElement(By.name("nickname")).clear();
+        driver.findElement(By.name("nickname")).sendKeys("testnickname");
+        driver.findElement(By.name("title")).click();
+        driver.findElement(By.name("title")).clear();
+        driver.findElement(By.name("title")).sendKeys("testtitle");
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).clear();
+        driver.findElement(By.name("address")).sendKeys("testaddress");
+        driver.findElement(By.name("company")).click();
+        driver.findElement(By.name("company")).clear();
+        driver.findElement(By.name("company")).sendKeys("testcompany");
+        driver.findElement(By.name("home")).click();
+        driver.findElement(By.name("home")).clear();
+        driver.findElement(By.name("home")).sendKeys("808080808");
+        driver.findElement(By.name("mobile")).click();
+        driver.findElement(By.name("mobile")).clear();
+        driver.findElement(By.name("mobile")).sendKeys("80808080808");
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys("test@test");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+        driver.findElement(By.linkText("home page")).click();
+    }
+
+    @AfterTest
+    public void tearDown(){
         driver.quit();
     }
+
+   /* public static boolean isAlertPresent(FirefoxDriver driver) {
+        try{
+            driver.switchTo().alert();
+            return true;
+        }catch (NoAlertPresentException e){
+            return false;
+        }
+    }*/
 }
