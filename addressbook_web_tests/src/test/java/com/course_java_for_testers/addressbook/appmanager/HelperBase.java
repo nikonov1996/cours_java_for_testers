c89d78040bfa5e162c6c531adc86ac1b10a7e879
Nikonov Vladislav <ibigmeat1996@gmail.com>
2019-04-10 11:14:12 +0700

    Оптимизация полей ввода

--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/appmanager/HelperBase.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/appmanager/HelperBase.java
@@ -14,15 +14,21 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(text);
+        if (text != null) {
+            String existingText = driver.findElement(locator).getAttribute("value");
+            if (! text.equals(existingText)) {
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
+        }
+
     }
 
     protected void click(By locator) {
         driver.findElement(locator).click();
     }
 
-    protected  void confirmDeletion() {
+    protected void confirmDeletion() {
         driver.switchTo().alert().accept();
     }
 }
