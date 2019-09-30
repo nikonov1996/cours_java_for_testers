7b055a0fe2ad3f861413707865fa29cd372dcbb5
Никонов Владислав <39374425+nikonov1996@users.noreply.github.com>
2019-09-30 13:06:57 +0700

    все тесты запускаются в одном браузере
    + переменная app теперь глобальная (static)
    + изменена аннотация на TestSuite (пакет тестов)

--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/TestBase.java
@@ -3,18 +3,20 @@ package com.course_java_for_testers.addressbook.tests;
 import com.course_java_for_testers.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp(){
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown(){
         app.stop();
     }
