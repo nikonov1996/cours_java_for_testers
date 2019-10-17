d6763ec99766789eeea6f689851c052be39d1598
Никонов Владислав <39374425+nikonov1996@users.noreply.github.com>
2019-10-17 11:21:15 +0700

    загружаем тестовые данные из файла

--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,15 +14,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> paramsForTest(){
+    public Iterator<Object[]> paramsForTest() throws IOException {
         ArrayList<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("src/test/resources/testdata.txt")));
+        String lineWithParams = bufferedReader.readLine();
+        while (lineWithParams != null){
+            String[] paramsObject = lineWithParams.split(";");
+            list.add(new Object[]{new GroupData().withName(paramsObject[0]).withHeader(paramsObject[1]).withFooter(paramsObject[2])});
+            lineWithParams = bufferedReader.readLine();
+        }
+
+/*  // Если хочется менять тестовые данные вручную в коде:
         list.add(new Object[]{new GroupData().withName("testname1").withHeader("testheader1").withFooter("testfooter1")});
         list.add(new Object[]{new GroupData().withName("testname2").withHeader("testheader2").withFooter("testfooter2")});
         list.add(new Object[]{new GroupData().withName("testname3").withHeader("testheader3").withFooter("testfooter3")});
         list.add(new Object[]{new GroupData().withName("testname4").withHeader("testheader4").withFooter("testfooter4")});
         list.add(new Object[]{new GroupData().withName("testname5").withHeader("testheader5").withFooter("testfooter5")});
+*/
         return list.iterator();
-    }
+    };
 
 
     @Test (dataProvider = "paramsForTest")
