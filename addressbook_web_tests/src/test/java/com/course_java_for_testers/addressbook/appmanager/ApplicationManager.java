73d925808e573317eb743c46a096cb12f890ac58
Никонов Владислав <39374425+nikonov1996@users.noreply.github.com>
2019-10-18 18:30:14 +0700

    добавлен файл конфигурации тестового стенда
    +необходимо чтобы запускать тесты на разных учетных записях и разных юрлах .

--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/appmanager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/appmanager/ApplicationManager.java
@@ -5,13 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.ReporterConfig;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver driver;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
@@ -22,9 +29,13 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+
+        String target = System.getProperty("target","local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
 
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             System.setProperty("webdriver.gecko.driver", "C:\\Geckodriver\\geckodriver.exe");
@@ -39,12 +50,15 @@ public class ApplicationManager {
         }
 
         driver.manage().window().maximize();
-        driver.navigate().to("http://localhost:122/addressbook");
+        driver.navigate().to(
+                properties.getProperty("web.baseURL"));
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
         sessionHelper = new SessionHelper(driver);
         contactHelper = new ContactHelper(driver);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(
+                properties.getProperty("web.adminLogin"),
+                properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/TestBase.java
@@ -8,12 +8,14 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+
 public class TestBase extends TestDataProvider {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUp(){
+    public void setUp() throws IOException {
         app.init();
     }
 
