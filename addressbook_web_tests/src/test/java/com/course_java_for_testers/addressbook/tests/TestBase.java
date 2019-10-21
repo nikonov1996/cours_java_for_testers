a30406d3f0f92094d51a31c5f81e4d65c12376ce
Никонов Владислав <39374425+nikonov1996@users.noreply.github.com>
2019-10-21 16:35:58 +0700

    Добавлена возможность логирования тестов
    + логи будут складываться в папку logs

--- addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/com/course_java_for_testers/addressbook/tests/TestBase.java
@@ -3,14 +3,20 @@ package com.course_java_for_testers.addressbook.tests;
 import com.course_java_for_testers.addressbook.appmanager.ApplicationManager;
 import com.course_java_for_testers.addressbook.model.TestDataProvider;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase extends TestDataProvider {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
@@ -19,8 +25,19 @@ public class TestBase extends TestDataProvider {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown(){
         app.stop();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method currentMethod, Object[] parametrs){
+        logger.info("Test '" + currentMethod.getName() + "' started..");
+        logger.info("Test's params: " + Arrays.asList(parametrs));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method currentMethod){
+        logger.info("Test '" + currentMethod.getName() + "' stopped");
     }
 }
